7cdfefca4e0dd0ea7cc22b808bde2e3f9561ea01
Nihal Mohammed <mnihal64@outlook.com>
2021-07-26 19:24:57 +0530

    Add business_address form to business details section

--- app/models/spree/address.rb
+++ app/models/spree/address.rb
@@ -10,7 +10,9 @@ module Spree
     has_one :enterprise, dependent: :restrict_with_exception
     has_many :shipments
 
-    validates :firstname, :lastname, :address1, :city, :country, presence: true
+    validates :address1, :city, presence: true
+    validates :firstname, :lastname, presence: true, unless: -> { :company.present? }
+    validates :company, presence: true, unless: -> { :firstname.present? || :lastname.present? }
     validates :zipcode, presence: true, if: :require_zipcode?
     validates :phone, presence: true, if: :require_phone?
 
